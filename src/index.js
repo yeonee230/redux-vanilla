6e18d70c634b3453b3a067733ea95bb9ba4e809f
SUYEON KIM <yeonee230@gmail.com>
2023-05-18 08:08:25 +0900

    subscribe

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import { legacy_createStore, dispatch } from 'redux';
+import { legacy_createStore } from 'redux';
 
 const plus = document.getElementById('plus');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
+
+let count = 0;
 
 //2. reducer는 data를 수정할 수 있는 함수
 const countModifier = (state = 0, action) => {
@@ -20,9 +22,20 @@ const countModifier = (state = 0, action) => {
 //1.store를 만든다. store는 데이터를 저장하는 곳. reducer를 넣어줘야함.
 const store = legacy_createStore(countModifier);
 
-//3. ✅ Action : redux에서 function을 부를 때 쓰는 두 번째 parameter 혹은 argument으로 reducer와 소통하기 위해
-//✅ Reducer에게 Action을 보내는 방법 : store.dispatch({key: value});
-store.dispatch({ type: 'PLUS' });
-store.dispatch({ type: 'PLUS' });
-store.dispatch({ type: 'MINUS' });
-console.log('Result: ',store.getState());
+/**4. ✅ Subscribe : store 안에 있는 변화 감지
+store.subscribe(func); // store안의 변화를 감지하면 func 실행
+*/
+number.innerHTML = 0;
+const onChange = () => {
+  number.innerHTML = store.getState();
+};
+store.subscribe(onChange);
+
+// 3. ✅ Action : redux에서 function을 부를 때 쓰는 두 번째 parameter 혹은 argument으로 reducer와 소통하기 위해
+// ✅ Reducer에게 Action을 보내는 방법 : store.dispatch({key: value});
+// store.dispatch({ type: 'PLUS' });
+// store.dispatch({ type: 'PLUS' });
+// store.dispatch({ type: 'MINUS' });
+// console.log('Result: ',store.getState());
+plus.addEventListener('click', () => store.dispatch({ type: 'PLUS' }));
+minus.addEventListener('click', () => store.dispatch({ type: 'MINUS' }));
